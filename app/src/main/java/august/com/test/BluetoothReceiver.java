2f395fe9450d22b7eb6481bb5a8f6106ffcd94a1
Lillian <weijingwen35@foxmail.com>
2018-08-25 21:56:26 +0800

    BlueTooth build

---
+++ app/src/main/java/august/com/test/BluetoothReceiver.java
@@ -0,0 +1,65 @@
+package august.com.test;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BluetoothReceiver extends BroadcastReceiver{
+
+    String pin = "1234";//为连接设备的初始密码
+    public BluetoothReceiver(){
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    // 广播接收器，当远程蓝牙设备被发现时候，回调函数onReceiver()会被执行
+
+        String action = intent.getAction();//得到 action
+        Log.e("action1=",action);
+        BluetoothDevice btDevice=null;// 创建一个蓝牙device对象
+        btDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);// 从Intent中获取设备对象
+
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) { // 发现设备
+            Log.e("发现设备：", "[" + btDevice.getName() + "]" + ":" + btDevice.getAddress());
+            if (btDevice.getName().contains("HC-02")) { // HC-02设备如果有多个，第一个搜到的那个会被执行。
+                if (btDevice.getBondState() == BluetoothDevice.BOND_NONE){
+                    Log.e("ywq","attemp to bond:"+"["+ btDevice.getName() + "]");
+                    try {
+                        //通过工具类ClsUtils调用createBond方法
+                        ClsUtils.creatBond(btDevice.getClass(), btDevice);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }else
+                Log.e("error", "Is faild");
+        }else if (action.equals("android.bluetooth.device.action.PAIRING_REQUEST")) {//再次得到的action，会等于PAIRING_REQUEST
+            Log.e("action2=", action);
+            if(btDevice.getName().contains("HC-02"))
+            {
+                Log.e("here", "OK");
+
+                try {
+
+                    //1.确认配对
+                    ClsUtils.setPairingConfirmation(btDevice.getClass(), btDevice, true);
+                    //2.终止有序广播
+                    Log.i("order...", "isOrderedBroadcast:"+isOrderedBroadcast()+",isInitialStickyBroadcast:"+isInitialStickyBroadcast());
+                    abortBroadcast();//如果没有将广播终止，则会出现一个一闪而过的配对框。
+                    //3.调用setPin方法进行配对...
+                    boolean ret = ClsUtils.setPin(btDevice.getClass(), btDevice, pin);
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }else
+                Log.e("提示信息", "这个设备不是目标蓝牙设备");
+
+        }
+    }
+
+}
--- app/src/main/java/august/com/test/MainActivity.java
+++ app/src/main/java/august/com/test/MainActivity.java
@@ -1,14 +1,40 @@
 package august.com.test;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
-    // 当第一次创建改Activity时回调该方法
+    // 定义组件
+    TextView T,AP,Tc,AF;
+    private Button autopairbtn=null;
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autopairbtn=(Button) findViewById(R.id.button1);
+        autopairbtn.setOnClickListener(this);
+
+    }
+
+    //设置按钮的监听方法
+    public void onClick(View arg0) {
+
+        if (!bluetoothAdapter.isEnabled())
+        {
+            bluetoothAdapter.enable();//异步的，不会等待结果，直接返回。
+        }else{
+            bluetoothAdapter.startDiscovery();
+        }
+
     }
 }
