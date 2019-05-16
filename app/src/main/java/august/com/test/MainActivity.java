73128c0bd461eafe9a14d7db0e3ede4a51516291
NnnLillian <weijingwen35@foxmail.com>
2019-05-16 16:10:08 +0800

    0516 格式调整

--- app/src/main/java/august/com/test/MainActivity.java
+++ app/src/main/java/august/com/test/MainActivity.java
@@ -22,6 +22,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -44,13 +45,13 @@ public class MainActivity extends Activity {
 
     //定义组件
     TextView statusLabel;
-    Button btnConnect,btnSend,btnQuit,btnOn,btnOff;
-    EditText etReceived,etSend;
-    TextView T,AP,Tc,AF,LS;
+    Button btnConnect, btnSend, btnQuit, btnOn, btnOff;
+    EditText etReceived, etSend;
+    TextView T, AP, Tc, AF, LS;
     Toolbar toolbar;
     Spinner spinner;
-    List<String>  data_list;
-    List<String>  address_list;
+    List<String> data_list;
+    List<String> address_list;
     ArrayAdapter<String> arr_adapter;
     AlertDialog.Builder builder;
 
@@ -69,27 +70,26 @@ public class MainActivity extends Activity {
 
 //    private static String address = "00:0C:BF:17:66:0F"; // <==要连接的目标蓝牙设备MAC地址
 
-    String address="";
+    String address = "";
 
-    private ReceiveThread rThread=null;  //数据接收线程
+    private ReceiveThread rThread = null;  //数据接收线程
 
     //接收到的字符串
-    String ReceiveData="";
-    String times="";
-    String pressure="";
-    String tempretures="";
-    String flow="";
+    String ReceiveData = "";
+    String times = "";
+    String pressure = "";
+    String tempretures = "";
+    String flow = "";
     double flows;
-    String LeakResult="";
+    String LeakResult = "";
     double LeakResults;
-    String ON="ON";
-    String OFF="OFF";
+    String ON = "ON";
+    String OFF = "OFF";
 
     Boolean btn_click = false;
     Boolean connect_click = false;
     Boolean enable_click = true;
     MyHandler handler;
-
 
 
     @Override
@@ -100,34 +100,33 @@ public class MainActivity extends Activity {
         Init();
         InitBluetooth();
 
-        handler=new MyHandler();
+        handler = new MyHandler();
 
         //判断蓝牙是否打开
-        if(!mBluetoothAdapter.isEnabled())
-        {
+        if (!mBluetoothAdapter.isEnabled()) {
             mBluetoothAdapter.enable();
         }
         mBluetoothAdapter.startDiscovery();
 
-        data_list=new ArrayList<String>();
-        address_list=new ArrayList<String>();
+        data_list = new ArrayList<String>();
+        address_list = new ArrayList<String>();
 
         // 寻找已经配对设备
         Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
-        if (devices.size()>0){
-            for (Iterator<BluetoothDevice> it = devices.iterator(); it.hasNext();){
-                BluetoothDevice device = (BluetoothDevice)it.next();
+        if (devices.size() > 0) {
+            for (Iterator<BluetoothDevice> it = devices.iterator(); it.hasNext(); ) {
+                BluetoothDevice device = (BluetoothDevice) it.next();
                 data_list.add(device.getName());
                 address_list.add(device.getAddress());
                 Log.e("设备：", "[" + device.getName() + "]" + ":" + device.getAddress());
             }
-        }else {
+        } else {
             builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Remind");
             builder.setMessage("Sorry, No paired bluetooth devices now, Please pair the bluetooth device first");
-            builder.setPositiveButton("OK",null);
+            builder.setPositiveButton("OK", null);
             builder.show();
-            Log.e("sorry","no device bonded");
+            Log.e("sorry", "no device bonded");
         }
 
         // 下拉菜单定义一个数组适配器，这个数组适配器就用到之前定义的data_list，装的都是list所添加的内容
@@ -137,25 +136,25 @@ public class MainActivity extends Activity {
         // 以上声明完毕后，加载适配器
         spinner.setAdapter(arr_adapter);
         // 为下拉列表设置各种点击事件，一响应菜单中的文本item被选中，用setOnItemSelectedListener
-        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3){
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
                 // TODO Auto-generated method stub
                 // 所执行事件
                 address = address_list.get(arg2);
-                Log.e("address: ",address);
-                Toast.makeText(getApplicationContext(),"选择的是"+arr_adapter.getItem(arg2), Toast.LENGTH_LONG).show();
+                Log.e("address: ", address);
+                Toast.makeText(getApplicationContext(), "选择的是" + arr_adapter.getItem(arg2), Toast.LENGTH_LONG).show();
             }
 
-            public void onNothingSelected(AdapterView<?> arg0){
-                Toast.makeText(getApplicationContext(),"nothing",Toast.LENGTH_LONG).show();
+            public void onNothingSelected(AdapterView<?> arg0) {
+                Toast.makeText(getApplicationContext(), "nothing", Toast.LENGTH_LONG).show();
             }
         });
 
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!btn_click && enable_click){
-                    if (address !=null){
+                if (!btn_click && enable_click) {
+                    if (address != null) {
                         btn_click = true;
                         //创建连接
                         new ConnectTask().execute(address);
@@ -167,8 +166,8 @@ public class MainActivity extends Activity {
                     } else {
                         statusLabel.setText("Nothing be selected");
                     }
-                }else {
-                    Toast.makeText(getApplicationContext(),"Don't click repeatedly",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Don't click repeatedly", Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -180,19 +179,17 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                if (btn_click){
-                    btn_click=false;
+                if (btn_click) {
+                    btn_click = false;
                     btnConnect.setActivated(btn_click);
                     enable_click = true;
                     btnConnect.setEnabled(enable_click);
                 }
-                if(btSocket!=null)
-                {
+                if (btSocket != null) {
                     try {
                         btSocket.close();
-                        btSocket=null;
-                        if(rThread!=null)
-                        {
+                        btSocket = null;
+                        if (rThread != null) {
                             rThread.join();
                         }
                         statusLabel.setText("No Connection");
@@ -213,7 +210,6 @@ public class MainActivity extends Activity {
                 }
 
 
-
             }
         });
 
@@ -231,11 +227,15 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                if (!connect_click){
-                    btnOn.setEnabled(enable_click);
-                    connect_click = true;
-                    new SendInfoTask().execute(ON);
-                    btnOn.setActivated(connect_click);
+                if (!connect_click) {
+                    if (enable_click) {
+                        connect_click = true;
+                        new SendInfoTask().execute(ON);
+                        btnOn.setActivated(connect_click);
+                    } else {
+                        statusLabel.setText("Illegal!\nPress Disconnect\nSelect again");
+                    }
+
                 }
 
             }
@@ -255,31 +255,28 @@ public class MainActivity extends Activity {
         });
     }
 
-    public void Init()
-    {
-        statusLabel=(TextView)this.findViewById(R.id.textView1);
-        T=(TextView) this.findViewById(R.id.stoppedTime);
-        AP=(TextView)this.findViewById(R.id.press);
-        Tc=(TextView)this.findViewById(R.id.degree);
-        AF=(TextView)this.findViewById(R.id.flowData);
-        LS=(TextView)this.findViewById(R.id.result);
-        btnConnect=(Button)this.findViewById(R.id.button1);
+    public void Init() {
+        statusLabel = (TextView) this.findViewById(R.id.textView1);
+        T = (TextView) this.findViewById(R.id.stoppedTime);
+        AP = (TextView) this.findViewById(R.id.press);
+        Tc = (TextView) this.findViewById(R.id.degree);
+        AF = (TextView) this.findViewById(R.id.flowData);
+        LS = (TextView) this.findViewById(R.id.result);
+        btnConnect = (Button) this.findViewById(R.id.button1);
 //        btnSend=(Button)this.findViewById(R.id.button2);
-        btnOn=(Button)this.findViewById(R.id.on);
-        btnOff=(Button)this.findViewById(R.id.off);
-        btnQuit=(Button)this.findViewById(R.id.button3);
-        toolbar=(Toolbar)this.findViewById(R.id.toolbar);
-        spinner=(Spinner)this.findViewById(R.id.spinner);
+        btnOn = (Button) this.findViewById(R.id.on);
+        btnOff = (Button) this.findViewById(R.id.off);
+        btnQuit = (Button) this.findViewById(R.id.button3);
+        toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        spinner = (Spinner) this.findViewById(R.id.spinner);
 //        etSend=(EditText)this.findViewById(R.id.editText1);
 //        etReceived=(EditText)this.findViewById(R.id.editText2);
     }
 
-    public void InitBluetooth()
-    {
+    public void InitBluetooth() {
         //得到一个蓝牙适配器
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if(mBluetoothAdapter == null)
-        {
+        if (mBluetoothAdapter == null) {
             Toast.makeText(this, "No supporting Bluetooth", Toast.LENGTH_LONG).show();
             finish();
             return;
@@ -295,8 +292,7 @@ public class MainActivity extends Activity {
     }
 
     //连接蓝牙设备的异步任务
-    class ConnectTask extends AsyncTask<String,String,String>
-    {
+    class ConnectTask extends AsyncTask<String, String, String> {
 
 
         @Override
@@ -322,7 +318,7 @@ public class MainActivity extends Activity {
 
                 } catch (IOException e2) {
 
-                    Log .e("error","ON RESUME: Unable to close socket during connection failure", e2);
+                    Log.e("error", "ON RESUME: Unable to close socket during connection failure", e2);
                     return "Socket failed";
                 }
 
@@ -347,7 +343,7 @@ public class MainActivity extends Activity {
             // TODO Auto-generated method stub
 
             //连接成功则启动监听
-            rThread=new ReceiveThread();
+            rThread = new ReceiveThread();
 
             rThread.start();
 
@@ -357,12 +353,10 @@ public class MainActivity extends Activity {
         }
 
 
-
     }
 
     //发送数据到蓝牙设备的异步任务
-    class SendInfoTask extends AsyncTask<String,String,String>
-    {
+    class SendInfoTask extends AsyncTask<String, String, String> {
 
         @Override
         protected void onPostExecute(String result) {
@@ -379,12 +373,11 @@ public class MainActivity extends Activity {
         protected String doInBackground(String... arg0) {
             // TODO Auto-generated method stub
 
-            if(btSocket==null)
-            {
-                return "No connection";
+            if (btSocket == null) {
+                return "No Connection";
             }
 
-            if(arg0[0].length()>0)//不是空白串
+            if (arg0[0].length() > 0)//不是空白串
             {
                 //String target=arg0[0];
 
@@ -408,18 +401,16 @@ public class MainActivity extends Activity {
 
 
     //从蓝牙接收信息的线程
-    class ReceiveThread extends Thread
-    {
+    class ReceiveThread extends Thread {
 
-        String buffer="";
+        String buffer = "";
 
         @Override
         public void run() {
 
-            while(btSocket!=null )
-            {
+            while (btSocket != null) {
                 //定义一个存储空间buff
-                byte[] buff=new byte[1024];
+                byte[] buff = new byte[1024];
                 try {
                     inStream = btSocket.getInputStream();
                     System.out.println("waitting for instream");
@@ -427,7 +418,7 @@ public class MainActivity extends Activity {
 //                    System.out.println("buff receive :"+buff.length);
 
 
-                    processBuffer(buff,1024);
+                    processBuffer(buff, 1024);
 
                     //System.out.println("receive content:"+ReceiveData);
                 } catch (IOException e) {
@@ -437,24 +428,19 @@ public class MainActivity extends Activity {
             }
         }
 
-        private void processBuffer(byte[] buff,int size)
-        {
-            int length=0;
-            for(int i=0;i<size;i++)
-            {
-                if(buff[i]>'\0')
-                {
+        private void processBuffer(byte[] buff, int size) {
+            int length = 0;
+            for (int i = 0; i < size; i++) {
+                if (buff[i] > '\0') {
                     length++;
-                }
-                else
-                {
+                } else {
                     break;
                 }
             }
 
 //			System.out.println("receive fragment size:"+length);
 
-            byte[] newbuff=new byte[length];  //newbuff字节数组，用于存放真正接收到的数据
+            byte[] newbuff = new byte[length];  //newbuff字节数组，用于存放真正接收到的数据
 
             if (length > 20) {
                 for (int j = 0; j < length; j++) {
@@ -470,17 +456,17 @@ public class MainActivity extends Activity {
 //String item[] = pureData.split(":");
 //                Log.e("item", item[1]);
 //                Log.e("item3",item[3]);
-                times = ReceiveData.substring(1,6);
-                String timeStr = ReceiveData.substring(0,7);
-                String pureData = ReceiveData.replace(timeStr,"");
+                times = ReceiveData.substring(1, 6);
+                String timeStr = ReceiveData.substring(0, 7);
+                String pureData = ReceiveData.replace(timeStr, "");
                 System.out.println(pureData);
-                String p = pureData.substring(pureData.indexOf(":")+1,pureData.indexOf("T"));
+                String p = pureData.substring(pureData.indexOf(":") + 1, pureData.indexOf("T"));
                 System.out.println(p.replace(" ", "0"));
                 pureData = pureData.substring(pureData.indexOf("T"));
-                String t = pureData.substring(pureData.indexOf(":")+1,pureData.indexOf("A"));
+                String t = pureData.substring(pureData.indexOf(":") + 1, pureData.indexOf("A"));
                 System.out.println(t.replace(" ", "0"));
                 pureData = pureData.substring(pureData.indexOf("A"));
-                String f = pureData.substring(pureData.indexOf(":")+1);
+                String f = pureData.substring(pureData.indexOf(":") + 1);
                 System.out.println(f.replace(" ", "0"));
                 pressure = p.replace(" ", "0");
                 tempretures = t.replace(" ", "0");
@@ -494,7 +480,7 @@ public class MainActivity extends Activity {
                 Message msg = Message.obtain();
                 msg.what = 1;
                 handler.sendMessage(msg);  //发送消息:系统会自动调用handleMessage( )方法来处理消息
-            }else {
+            } else {
                 Message msg = Message.obtain();
                 msg.what = 2;
                 handler.sendMessage(msg);
@@ -504,14 +490,13 @@ public class MainActivity extends Activity {
     }
 
 
-
     //更新界面的Handler类
-    class MyHandler extends Handler{
+    class MyHandler extends Handler {
 
         @Override
         public void handleMessage(Message msg) {
 
-            switch(msg.what){
+            switch (msg.what) {
                 case 1:
 //                    etReceived.setText(ReceiveData);
                     T.setText(times);
@@ -532,11 +517,10 @@ public class MainActivity extends Activity {
         super.onDestroy();
 
         try {
-            if(rThread!=null)
-            {
+            if (rThread != null) {
 
                 btSocket.close();
-                btSocket=null;
+                btSocket = null;
 
                 rThread.join();
             }
